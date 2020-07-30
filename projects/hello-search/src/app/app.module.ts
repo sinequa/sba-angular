2155d81b75c192fbf41436e6b89feb0cc5002698
bell <bell@sinequa.com>
2020-07-30 11:59:35 +0100

    add comments to the providers section in app modules

--- projects/hello-search/src/app/app.module.ts
+++ projects/hello-search/src/app/app.module.ts
@@ -41,10 +41,27 @@ export const startConfig: StartConfig = {
         AppComponent,
     ],
     providers: [
+        // Provides an APP_INITIALIZER which will fetch application configuration information from the Sinequa
+        // server automatically at startup using the application name specified in the URL (app[-debug]/<app-name>).
+        // This allows an application to avoid hard-coding parameters in the StartConfig but requires that the application
+        // be served from the an app[-debug]/<app name> URL.
         // {provide: APP_INITIALIZER, useFactory: StartConfigInitializer, deps: [StartConfigWebService], multi: true},
+
+        // Provides the Angular LocationStrategy to be used for reading route state from the browser's URL. Currently
+        // only the HashLocationStrategy is supported by Sinequa.
         {provide: LocationStrategy, useClass: HashLocationStrategy},
+
+        // Provides an HttpInterceptor to handle user login. The LoginInterceptor handles HTTP 401 responses
+        // to Sinequa web service requests and initiates the login process.
         {provide: HTTP_INTERCEPTORS, useClass: LoginInterceptor, multi: true},
+
+        // Provides an HttpInterceptor that offers a centralized location through which all client-side
+        // audit records pass. An application can replace AuditInterceptor with a subclass that overrides
+        // the updateAuditRecord method to add custom audit information to the records.
         {provide: HTTP_INTERCEPTORS, useClass: AuditInterceptor, multi: true},
+
+        // Provides an HttpInterceptor that automatically processes any notifications specified in the $notifications
+        // member of the response body to any Sinequa web service requests.
         {provide: HTTP_INTERCEPTORS, useClass: NotificationsInterceptor, multi: true},
     ],
     bootstrap: [
