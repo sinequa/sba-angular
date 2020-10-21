ea4036acdd491a88885f0aeedd8d97a1cbd1dcbc
Olivier TOME <Olivier.TOME@sinequa.com>
2020-10-21 18:01:23 +0200

    fix: selection with concepts

---
+++ projects/components/__tests__/mocks/start.config.ts
@@ -0,0 +1,8 @@
+import {StartConfig} from '@sinequa/core/web-services';
+
+export const startConfig: StartConfig = {
+  app: "training",
+  production: false,
+  autoSAMLProvider: "identity-dev",
+  auditEnabled: true
+};
---
+++ projects/components/__tests__/stubs/appService.stub.ts
@@ -0,0 +1,11 @@
+export const AppServiceStub = {
+  events: {subscribe: f => f},
+  ccquery: {
+    name: "ccquery_name",
+    tabSearch: {tabs: {find: () => ({})}},
+    allowEmptySearch: false
+  },
+  defaultCCQuery: {name: "default_ccquery_name"},
+  getCCQuery: name => ({name: "def"}),
+  parseExpr: expression => ({field: {}, value: {}, not: {}})
+};
