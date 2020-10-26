15560a6143161d471967b91bc61f335117f6d183
Olivier TOME <Olivier.TOME@sinequa.com>
2020-10-26 11:31:03 +0100

    chore(BsFacetFilters): refacto

--- projects/components/facet/bootstrap/facet-filters/facet-filters.ts
+++ projects/components/facet/bootstrap/facet-filters/facet-filters.ts
@@ -1,11 +1,10 @@
-import {Component, Input, OnChanges, SimpleChanges} from "@angular/core";
-import {Results, Select, AggregationItem, TreeAggregationNode} from "@sinequa/core/web-services";
-import {AppService, Expr} from "@sinequa/core/app-utils";
-import {Utils} from "@sinequa/core/base";
+import {Component, Input, OnChanges} from "@angular/core";
+import {Results} from "@sinequa/core/web-services";
 import {FacetService} from "../../facet.service";
-import { Action } from "@sinequa/components/action";
-import {SearchService} from "@sinequa/components/search";
-import { FacetConfig } from '../facet-multi/facet-multi.component';
+import {Action} from "@sinequa/components/action";
+import {FacetConfig} from '../facet-multi/facet-multi.component';
+import {BsFacetList} from '../facet-list/facet-list';
+import {BsFacetTree} from '../facet-tree/facet-tree';
 
 @Component({
     selector: "sq-facet-filters",
@@ -16,37 +15,34 @@ export class BsFacetFilters implements OnChanges {
     @Input() results: Results;
     @Input() facets: FacetConfig[];
     @Input() enableAddFacet = true;
-    @Input() itemCount: number = 5;
-    
+
     @Input() autoAdjust: boolean = true;
     @Input() autoAdjustBreakpoint: string = 'xl';
     @Input() rightAligned: boolean = false;
     @Input() size: string;
 
-    filters : Action[];
-    hidden: boolean;
+    filters: Action[] = [];
+    hidden: boolean = false;
 
     facetStatus = {
-        add : {
-            title : "msg#facet.filters.add",
-            icon  : "fas fa-plus"
+        add: {
+            title: "msg#facet.filters.add",
+            icon: "fas fa-plus"
         },
-        remove : {
-            title : "msg#facet.filters.remove",
-            icon  : "fas fa-minus"
+        remove: {
+            title: "msg#facet.filters.remove",
+            icon: "fas fa-minus"
         }
     };
 
     constructor(
-        public appService: AppService,
-        public searchService: SearchService,
-        private facetService : FacetService
+        private facetService: FacetService
     ) {
         this.hidden = false;
         this.filters = [];
     }
 
-    ngOnChanges(changes : SimpleChanges) {
+    ngOnChanges() {
         if(!!this.results)
             this.buildFilters();
 
@@ -54,187 +50,61 @@ export class BsFacetFilters implements OnChanges {
             this.hidden=true;
     }
 
-    //Returns the top 5 element for a given aggregation as an Action object
-    getChildren(CCFacetItem: FacetConfig, previousActions: Action[]): Action[] {
-
-        let childrenActions: Action[] = [];
-        const aggregationName = CCFacetItem.aggregation;
-        const facetName = CCFacetItem.name || aggregationName;
-        const aggregation = this.results.aggregations.find(x => x.name === aggregationName);
-
-        if(Utils.isUndefined(aggregation)) return [];
-
-        // Get facet data
-        let items: AggregationItem[] | undefined;
-        if (aggregation.isTree) {
-            const _aggregation = this.facetService.getAggregation(aggregationName, this.results, {facetName});
-            if (_aggregation) {
-                items = _aggregation.items;
-            }
-        }
-        else {
-            const _aggregation = this.facetService.getAggregation(aggregationName, this.results);
-            if (_aggregation) {
-                items = _aggregation.items;
-            }
-        }
-
-        if(Utils.isUndefined(items)) return [];
-
-        items = this.unpackChildren(items); // Flattening tree items
-
-        // Remove already filtered iteams
-        items = items.filter(item => previousActions.find(a => a.name === (item.value as string)) === undefined); // Filter out items which are already selected
-
-        if(items.length === 0) return [];
-
-        // Keep top values
-        const topValues = (items.slice(0,this.itemCount).length === 0) ?  items : items.slice(0,this.itemCount);
-
-        // Create select actions
-        childrenActions = topValues.map(item => {
-            const display : string = (item.display == null) ? item.value as string : item.display;
-            const count : string = item.count? " ("+item.count+")" : "";
-            return new Action({
-                name: item.value as string,
-                text: display,
-                title: "Select "+display+count,
-                action: () => {
-                    const _aggregation = this.results.aggregations.find(x => x.name === aggregationName);
-                    if (_aggregation) {
-                        this.facetService.addFilterSearch(facetName, _aggregation, item);
-                    }
-                },
-            });
-        }, this);
-
-        return childrenActions;
-    }
-
-    //Build filters bar : getting children and adding extra option (add/clear filter)
-    buildFilters(){
+    /**
+     * Build filters bar actions
+     */
+    private buildFilters() {
 
         // For each facet
-        this.facets.forEach((facet : FacetConfig) => {
+        this.filters = this.facets.map((facet: FacetConfig) => {
 
-            const childrenActions : Action[] = [];
+            const children = [
+                new Action({
+                    component: (facet.type === 'list') ? BsFacetList : BsFacetTree,
+                    componentInputs: {results: this.results, name: facet.name, aggregation: facet.aggregation, searchable: facet.searchable, displayActions: true}
+                }),
+                new Action({separator: true})
+            ];
 
-            if(this.searchService.query.select)
-                this.searchService.query.select
-                    .filter(s => s.facet === facet.name) // Look for this facet in the selects
-                    .map(s => this.removeSelect(s)) // Make a Remove select action for this facet
-                    .forEach(a => childrenActions.push(a)); // Add it to the list of actions for this facet
-
-            if(childrenActions.length > 0)
-                childrenActions.push(new Action({ separator: true }));
-
-            this.getChildren(facet, childrenActions)
-                .forEach(a => childrenActions.push(a));
-
-            if(childrenActions.length === 0){
-                return;
-            }
-
-            if(this.enableAddFacet) {
-
-                childrenActions.push(new Action({ separator: true }));
-
+            if (this.enableAddFacet) {
                 const isOpened = this.facetService.isFacetOpened(facet.name);
 
-                childrenActions.push(new Action({
+                children.push(new Action({
                     name: "toggle-facet",
-                    icon: isOpened? this.facetStatus.remove.icon : this.facetStatus.add.icon,
-                    text: isOpened? this.facetStatus.remove.title : this.facetStatus.add.title,
-                    title: isOpened? this.facetStatus.remove.title : this.facetStatus.add.title,
+                    icon: isOpened ? this.facetStatus.remove.icon : this.facetStatus.add.icon,
+                    text: isOpened ? this.facetStatus.remove.title : this.facetStatus.add.title,
+                    title: isOpened ? this.facetStatus.remove.title : this.facetStatus.add.title,
                     data: facet,
                     action: (ActionItem: Action, $event: UIEvent) => {
-                        this.onClickToggleFacet(ActionItem,$event);
+                        this.onClickToggleFacet(ActionItem, $event);
                     }
                 }));
-
             }
 
-            this.updateOrInitFilter(new Action({
+            return new Action({
                 name: facet.name,
                 text: facet.title,
                 title: facet.title,
                 icon: facet.icon,
-                children: childrenActions,
-            }),this.filters.find(x => x.name === facet.name));
-
-            return;
+                children: children
+            });
         });
     }
 
-    onClickToggleFacet(item: Action, $event: UIEvent){
+    onClickToggleFacet(item: Action, $event: UIEvent) {
 
         const facet = item.data as any;
 
-        if(item.title === this.facetStatus.add.title){
+        if (item.title === this.facetStatus.add.title) {
             item.text = item.title = this.facetStatus.remove.title;
             item.icon = this.facetStatus.remove.icon;
-            this.facetService.addFacet({name:facet.name, position:0, hidden:false, expanded:true, view:""});
+            this.facetService.addFacet({name: facet.name, position: 0, hidden: false, expanded: true, view: ""});
         }
-        else{
-            item.text = item.title =  this.facetStatus.add.title;
+        else {
+            item.text = item.title = this.facetStatus.add.title;
             item.icon = this.facetStatus.add.icon;
-            this.facetService.removeFacet({name:facet.name, position:0, hidden:false, expanded:true, view:""});
+            this.facetService.removeFacet({name: facet.name, position: 0, hidden: false, expanded: true, view: ""});
         }
-
-    }
-
-    unpackChildren(items: AggregationItem[]): AggregationItem[] {
-        const newItems: AggregationItem[] = [];
-        items.forEach(item => {
-            if ((item as TreeAggregationNode).items) {
-                const unpackedItems = this.unpackChildren((item as TreeAggregationNode).items);
-                unpackedItems
-                    .forEach(upi => {
-                        upi.display = (item.display || item.value) + " â€º "+ (upi.display || upi.value);
-                        //upi.value = "/"+item.value+(upi.value.startsWith("/")? upi.value : "/"+upi.value+"/*");
-                        newItems.push(upi);
-                    });
-            }
-            else {
-                newItems.push(Utils.copy(item));
-            }
-        });
-        return newItems;
-    }
-
-    // TODO: Refactor to avoid unclear pop/push
-    updateOrInitFilter(item: Action, current: Action | undefined): void {
-        const index = current && this.filters.indexOf(current);
-        if(current && index !== -1) {
-            if(this.enableAddFacet){
-                item.children.pop();
-                const child = current.children.pop();
-                if (child) {
-                    item.children.push(child);
-                }
-            }
-            this.filters[this.filters.indexOf(current)] = item;
-            return;
-        }
-        this.filters.push(item);
-    }
-
-    removeSelect(select: Select): Action {
-
-        const expr = this.appService.parseExpr(select.expression) as Expr;
-        const display = expr.display || expr.value;
-
-        return new Action({
-            name: expr.value,
-            text: display,
-            title: "Unselect "+display,
-            icon: this.facetStatus.remove.icon,
-            action: () => {
-                if (this.searchService.query.select) {
-                    this.searchService.removeSelect(this.searchService.query.select.indexOf(select));
-                }
-            },
-        });
     }
 
 }
