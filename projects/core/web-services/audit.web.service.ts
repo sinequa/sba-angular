bcf2f23b8722d33d4ac31462e53a3aa90d6dd913
Jimmy Yeh <jimmy.yeh@sinequa.com>
2021-03-24 16:59:02 +0100

    ES-11060 #done Audit timeline usage

--- projects/core/web-services/audit.web.service.ts
+++ projects/core/web-services/audit.web.service.ts
@@ -60,6 +60,7 @@ export const enum AuditEventType {
     Search_Select_AnalyticsItem = "Search_Select_AnalyticsItem",
     Search_Sort = "Search_Sort",
     Search_Exit_Logout = "Search_Exit_Logout",
+    Search_Timeline_Usage = "Search_Timeline_Usage",
 
     SavedQuery_Add = "SavedQuery_Add",
     SavedQuery_Delete = "SavedQuery_Delete",
