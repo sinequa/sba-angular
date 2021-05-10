cefb056cf8a1bd348cfef79a6be60a8e92653f0d
Eric Leibenguth <leibenguth@sinequa.com>
2021-05-10 11:15:43 +0200

    New service to use the query intent web service

--- projects/core/web-services/public-api.ts
+++ projects/core/web-services/public-api.ts
@@ -24,4 +24,5 @@ export * from "./suggest-field.web.service";
 export * from "./suggest-query.web.service";
 export * from "./user-ratings.web.service";
 export * from "./user-settings.web.service";
+export * from "./queryintent.service";
 export * from "./web-services.module";
