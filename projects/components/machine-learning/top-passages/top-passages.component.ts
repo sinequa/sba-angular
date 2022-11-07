806c1be53c94508802b16ce9599779f0b0a901e1
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2022-11-07 14:27:56 +0100

    add lineNumber input for passages + add expand icon

--- projects/components/machine-learning/top-passages/top-passages.component.ts
+++ projects/components/machine-learning/top-passages/top-passages.component.ts
@@ -1,14 +1,9 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from "@angular/core";
+import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, Output } from "@angular/core";
 import { Record, Results } from "@sinequa/core/web-services";
 import { AbstractFacet } from '@sinequa/components/facet';
 import { TopPassage } from "@sinequa/core/web-services/models/top-passage";
 import { BehaviorSubject } from "rxjs";
-
-export interface TopPassageConfig {
-  showTitle?: boolean;
-  showSource?: boolean;
-  lineClamp?: number;
-}
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'sq-top-passages',
@@ -16,6 +11,31 @@ export interface TopPassageConfig {
   styles: [`
 .card-body > div {
   cursor: pointer;
+}
+
+.sq-text-ellipsis {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: var(--line-clamp, 3);
+  -webkit-box-orient: vertical;
+  white-space: normal;
+
+  &.expanded {
+    --line-clamp: 0;
+  }
+}
+
+.btn-toggle {
+  position: relative;
+  top: .2rem;
+  opacity: 1;
+  width: 4%;
+  color: rgba(0,0,0,.5);
+
+  &--hidden {
+    opacity: 0;
+  }
 }
   `],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -34,7 +54,12 @@ export class TopPassagesComponent extends AbstractFacet {
     this.pageNumber = this.itemsPerPage === 1 ? this.passages.length : Math.floor(this.passages.length / this.itemsPerPage) + 1;
   }
   @Input() collapsed: boolean;
-  @Input() itemsPerPage: number = 3;
+
+  // Number of passages per page
+  @Input() itemsPerPage = 3;
+
+  // Number of lines displayed for each passages text
+  @Input() lineNumber = 3;
 
   @Output() previewOpened = new EventEmitter<TopPassage>();
   @Output() titleClicked = new EventEmitter<{ item: TopPassage, isLink: boolean }>();
@@ -43,6 +68,10 @@ export class TopPassagesComponent extends AbstractFacet {
   page: number;
   pageNumber: number;
   currentPassages$: BehaviorSubject<TopPassage[]> = new BehaviorSubject<TopPassage[]>([]);
+  selected: number;
+  
+  // Used to know if we should display the expand chevron
+  MIN_HEIGHT: number;
 
   get currentPage() {
     return this.page;
@@ -54,6 +83,7 @@ export class TopPassagesComponent extends AbstractFacet {
     this.currentPassages$.next(this.passages.slice(index, index + this.itemsPerPage));
   }
 
+  // Get the range of passages displayed to display in the pagination
   get currentRange() {
     if (this.itemsPerPage === 1) {
       return this.currentPage + 1;
@@ -64,15 +94,39 @@ export class TopPassagesComponent extends AbstractFacet {
     return from < to ? `${from}-${to}` : String(from);
   }
 
-  constructor() {
-    super();
+  // Used to handle dynamically the lines number per passage
+  @HostBinding("attr.style")
+  public get valueAsStyle(): any {
+    return this.sanitizer.bypassSecurityTrustStyle(`--line-clamp: ${this.lineNumber}`);
   }
 
+  constructor(private sanitizer: DomSanitizer) {
+    super();
+
+    this.MIN_HEIGHT = this.setMinHeight();
+  }
+
+  // Expand one passage's text
+  expand(passage: TopPassage) {
+    if (this.selected !== passage.id) {
+      this.selected = passage.id;
+    } else {
+      this.selected = -1;
+    }
+  }
+
+  // Open the mini preview on text click
   openPreview(passage: TopPassage) {
     this.previewOpened.next(passage);
   }
 
+  // Open the big preview on title click
   onTitleClicked(isLink: boolean, passage: TopPassage) {
     this.titleClicked.next({ item: passage, isLink });
+  }
+
+  // Calculation of a line height
+  private setMinHeight(): number {
+    return 24 * (this.lineNumber === 0 ? 1000 : this.lineNumber || 1);
   }
 }
