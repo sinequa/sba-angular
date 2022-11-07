8869ab66d53dfbbf576d2545c98404f63d8a0051
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2022-11-07 16:36:26 +0100

    make Passages tab only visible when there are some

--- projects/vanilla-search/src/app/preview/preview.component.ts
+++ projects/vanilla-search/src/app/preview/preview.component.ts
@@ -225,12 +225,9 @@ export class PreviewComponent implements OnInit, OnChanges, OnDestroy {
         previewData => {
           this.previewData = previewData;
           const url = previewData?.documentCachedContentUrl;
-          if(this.appService.isNeural() && !this.subpanels.includes("passages")) {
+          if(previewData.highlightsPerCategory['matchingpassages']?.values.length && !this.subpanels.includes("passages")) {
             this.subpanels.unshift("passages");
-            // Set subpanel as passages if there are some matching passages
-            if (previewData.highlightsPerCategory['matchingpassages']?.values.length) {
-              this.subpanel = "passages";
-            }
+            this.subpanel = "passages";
             this.minimapType = "matchingpassages";
           }
           // Manage splitted documents
