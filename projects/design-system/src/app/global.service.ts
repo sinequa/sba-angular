898fc01b1440018872b45cb312ea537b1c29ac55
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2023-01-12 16:15:58 +0100

    remove record mocks from doc app

--- projects/design-system/src/app/global.service.ts
+++ projects/design-system/src/app/global.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { SearchService } from '@sinequa/components/search';
 import { Query } from '@sinequa/core/app-utils';
 import { Results } from '@sinequa/core/web-services';
+import { Record } from "@sinequa/core/web-services";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ export class GlobalService {
 
   query: Query = new Query('training_query');
   results: Results;
+  record: Record;
 
   constructor(private searchService: SearchService) {
     this.search();
@@ -21,6 +23,10 @@ export class GlobalService {
       .subscribe(results => {
         console.log('results', results);
         this.results = results;
+        if (results?.records?.length) {
+          this.record = results.records[0];
+          console.log('record', this.record);
+        }
       });
   }
 }
--- projects/design-system/src/app/modules/preview/facet-preview/facet-preview.component.ts
+++ projects/design-system/src/app/modules/preview/facet-preview/facet-preview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '@sinequa/components/search';
-import { RECORD } from 'src/mocks/record';
+import { GlobalService } from '../../../global.service';
 
 @Component({
   selector: 'app-facet-preview',
@@ -8,11 +8,8 @@ import { RECORD } from 'src/mocks/record';
 })
 export class FacetPreviewComponent implements OnInit {
 
-  record = RECORD;
-
-  constructor(public searchService: SearchService) {
-
-  }
+  constructor(public searchService: SearchService,
+    public globalService: GlobalService) {}
 
   ngOnInit(): void {
   }
