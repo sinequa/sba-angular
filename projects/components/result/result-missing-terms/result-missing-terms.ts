9c47987d7a34765c7c207befb3c00d1ff6f3876d
Leibenguth <leibenguth@sinequa.com>
2023-02-09 16:16:59 +0100

    Added "Must include" option to missing terms component

--- projects/components/result/result-missing-terms/result-missing-terms.ts
+++ projects/components/result/result-missing-terms/result-missing-terms.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component, Input, OnChanges} from "@angular/core";
+import { SearchService } from "@sinequa/components/search";
 import {Utils} from "@sinequa/core/base";
 import {Record} from "@sinequa/core/web-services";
 
@@ -12,9 +13,21 @@ export class ResultMissingTerms implements OnChanges {
     @Input() record: Record;
     missingTerms: string[];
 
+    constructor(
+        public searchService: SearchService
+    ) {}
+
     ngOnChanges() {
         this.missingTerms = this.record.termspresence
             ?.filter(tp => Utils.eqNC(tp.presence, "missing"))
             .map(tp => tp.term) || [];
+    }
+
+    mustInclude(term: string) {
+        if(this.searchService.query.text) {
+            this.searchService.query.text = this.searchService.query.text.replace(new RegExp(`\\b${term}\\b`, 'gi'), "");
+        }
+        this.searchService.query.addConcepts([term], '+');
+        this.searchService.search();
     }
 }
