f41c9996852f261308477976e125ecb5b10959dc
Leibenguth <leibenguth@sinequa.com>
2023-03-06 14:52:00 +0100

    Added new plugin and conversational interface

--- projects/components/machine-learning/summary.component.ts
+++ projects/components/machine-learning/summary.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 import { JsonMethodPluginService, TopPassage } from "@sinequa/core/web-services";
 import { SearchService } from "@sinequa/components/search";
-import { map, Observable, tap } from "rxjs";
+import { BehaviorSubject, map, tap } from "rxjs";
 
 export interface SummarizerConfig {
   extendBefore: number;
@@ -19,6 +19,14 @@ export const defaultConfig: SummarizerConfig = {
   promptAfter: ""
 }
 
+export type OpenAIModel = "OpenAITextDavinci3" | "OpenAIgpt35Turbo";
+
+export type OpenAIModelMessage = {
+  role: string;
+  content: string;
+  display: boolean;
+}
+
 @Component({
   selector: 'sq-summary',
   template: `
@@ -26,21 +34,52 @@ export const defaultConfig: SummarizerConfig = {
     <span class="visually-hidden">Loading...</span>
   </div>
 
-  <div *ngIf="summary$ | async as summary">
-    <div><b>Generated summary:</b></div>
-    <p class="m-0" [innerHTML]="summary"></p>
-  </div>
+  <ul *ngIf="messages$ | async as messages" class="list-group list-group-flush">
+
+    <ng-container *ngFor="let message of messages; let i = index">
+      <li class="list-group-item border-bottom d-flex" *ngIf="message.display">
+        <span class="col-2 fw-bold text-end pe-3">{{message.role | titlecase}}</span>
+        <p [innerHTML]="message.content" class="col-10 mb-0"></p>
+      </li>
+    </ng-container>
+
+    <li class="list-group-item d-flex" [ngClass]="{'align-items-center': !loadingAnswer, 'border-bottom': loadingAnswer}">
+      <span class="col-2 fw-bold text-end pe-3">User</span>
+      <input type="text" class="form-control" placeholder="Your answer" autofocus
+        [(ngModel)]="question"
+        (keyup.enter)="submitQuestion()"
+        (keyup.shift.enter)="submitQuestion()"
+        *ngIf="!loadingAnswer">
+      <span class="col-10" *ngIf="loadingAnswer">{{question}}</span>
+    </li>
+
+    <li class="list-group-item" *ngIf="loadingAnswer">
+      <div class="spinner-grow text-success d-block mx-auto my-5" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </li>
+  </ul>
   `,
+  styles: [`
+  ul.list-group {
+    max-height: 100vh;
+    font-size: 0.875rem;
+  }
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SummaryComponent implements OnChanges {
   @Input() passages?: TopPassage[];
   @Input() config = defaultConfig;
+  @Input() model: OpenAIModel = "OpenAIgpt35Turbo";
 
-  @Output() summary = new EventEmitter<string>();
+  @Output() data = new EventEmitter<OpenAIModelMessage>();
 
   loading = false;
-  summary$?: Observable<string>;
+  loadingAnswer = false;
+  messages$ = new BehaviorSubject<OpenAIModelMessage[]|undefined>([]);
+
+  question = '';
 
   constructor(
     public jsonMethodWebService: JsonMethodPluginService,
@@ -49,18 +88,23 @@ export class SummaryComponent implements OnChanges {
 
   ngOnChanges() {
     if(this.passages) {
-      this.fetchSummary(this.passages);
+      this.fetchData(this.passages);
     }
     else {
-      delete this.summary$;
+      this.messages$.next(undefined);
       this.loading = false;
     }
   }
 
-  private fetchSummary(passages: TopPassage[]): void {
+  submitQuestion() {
+    if(this.question.trim() && this.messages$.value) {
+      this.fetchAnswer(this.question.trim(), this.messages$.value);
+    }
+  }
+
+  private fetchData(passages: TopPassage[]): void {
     this.loading = true;
     const data = {
-      debug: true,
       ...this.config,
       removeOverlap: true,
       removeDuplicates: true,
@@ -69,10 +113,28 @@ export class SummaryComponent implements OnChanges {
       queryText: this.searchService.query.text || '',
       passages: passages.map(p => ({docId: p.recordId, passageIndex: p.id, index: p.index}))
     }
-    this.summary$ = this.jsonMethodWebService.post("OpenAITextDavinci3", data).pipe(
-      map(res => res.summary),
-      tap(summary => this.summary.emit(summary)),
+    this.jsonMethodWebService.post(this.model, data).pipe(
+      map((res: {messagesHistory: OpenAIModelMessage[]}) => res.messagesHistory),
+      tap(res => this.messages$.next(res)),
+      tap(res => this.data.emit(res.at(-1))),
       tap(() => this.loading = false)
-    );
+    ).subscribe();
+  }
+
+  private fetchAnswer(question: string, previousMessages: OpenAIModelMessage[]) {
+    this.loadingAnswer = true;
+    const data = {
+      promptInsertBeforePassages: question,
+      previousMessages,
+      passages: [],
+      queryText: '...'
+    }
+    this.jsonMethodWebService.post(this.model, data).pipe(
+      map((res: {messagesHistory: OpenAIModelMessage[]}) => res.messagesHistory.filter(m => m.display)),
+      tap(res => this.messages$.next(res)),
+      tap(res => this.data.emit(res.at(-1))),
+      tap(() => this.loadingAnswer = false),
+      tap(() => this.question = '')
+    ).subscribe();
   }
 }
