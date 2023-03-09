f30a82f06cccd84a4308339c5fc9ef6f84cef256
Leibenguth <leibenguth@sinequa.com>
2023-03-09 19:01:50 +0100

    New API and parameters

--- projects/components/machine-learning/summary-settings.component.ts
+++ projects/components/machine-learning/summary-settings.component.ts
@@ -5,7 +5,9 @@ import { defaultSummarizerConfig, SummarizerConfig } from "./summary.component";
   selector: 'sq-summary-settings',
   template: `
   <div class="card-body small">
-    <h5>General parameters</h5>
+
+    <h5>Model parameters</h5>
+
     <div class="mb-2">
       <label for="temperature" class="form-label">Temperature: {{config.modelTemperature}}</label>
       <input type="range" class="form-range form-range-sm" min="0" max="2" step="0.1" id="temperature" [(ngModel)]="config.modelTemperature">
@@ -18,19 +20,27 @@ import { defaultSummarizerConfig, SummarizerConfig } from "./summary.component";
       <label for="max-tokens" class="form-label">Max tokens: {{config.modelMaxTokens}}</label>
       <input type="range" class="form-range form-range-sm" min="1" max="2048" step="1" id="max-tokens" [(ngModel)]="config.modelMaxTokens">
     </div>
+    <div class="form-check form-switch mb-3">
+      <input class="form-check-input" type="checkbox" role="switch" id="prompt-protection" [(ngModel)]="config.promptProtection">
+      <label class="form-check-label" for="prompt-protection">Prompt protection (prevent ChatGPT from answering question unrelated to the context)</label>
+    </div>
 
     <h5>Parameters specific to summarization</h5>
-    <div class="form-check mb-1">
-      <input class="form-check-input" type="checkbox" id="remove-dups" [(ngModel)]="config.removeDuplicates">
-      <label class="form-check-label" for="remove-dups">Remove duplicates</label>
-    </div>
-    <div class="form-check mb-1">
-      <input class="form-check-input" type="checkbox" id="remove-overlap" [(ngModel)]="config.removeOverlap">
-      <label class="form-check-label" for="remove-overlap">Remove overlap</label>
+
+    <div class="mb-2">
+      <label for="sum-model" class="form-label">Preferred model:</label>
+      <select class="form-select" id="sum-model" [(ngModel)]="config.summarizationModel">
+        <option>Davinci3</option>
+        <option>GPT35Turbo</option>
+      </select>
     </div>
     <div class="mb-2">
-      <label for="top-passages" class="form-label">Number of top passages: {{config.topPassages}}</label>
-      <input type="range" class="form-range form-range-sm" min="1" max="10" step="1" id="top-passages" [(ngModel)]="config.topPassages">
+      <label for="prompt-before" class="form-label">Prompt inserted before the paragraphs</label>
+      <textarea class="form-control form-control-sm" id="prompt-before" rows="6" [(ngModel)]="config.promptInsertBeforePassages"></textarea>
+    </div>
+    <div class="mb-2">
+      <label for="top-passages" class="form-label">Number of top passages: {{config.top}}</label>
+      <input type="range" class="form-range form-range-sm" min="1" max="10" step="1" id="top-passages" [(ngModel)]="config.top">
     </div>
     <div class="mb-2">
       <label for="extend-before" class="form-label">Number of passages to prepend before: {{config.extendBefore}}</label>
@@ -40,21 +50,13 @@ import { defaultSummarizerConfig, SummarizerConfig } from "./summary.component";
       <label for="extend-after" class="form-label">Number of passages to append after: {{config.extendAfter}}</label>
       <input type="range" class="form-range form-range-sm" min="0" max="3" step="1" id="extend-after" [(ngModel)]="config.extendAfter">
     </div>
-    <div class="mb-2">
-      <label for="prompt-before" class="form-label">Promp inserted before the paragraphs</label>
-      <textarea class="form-control form-control-sm" id="prompt-before" rows="4" [(ngModel)]="config.promptInsertBeforePassages"></textarea>
-    </div>
-    <div class="mb-2">
-      <label for="prompt-after" class="form-label">Promp inserted after the paragraphs</label>
-      <textarea class="form-control form-control-sm" id="prompt-after" rows="4" [(ngModel)]="config.promptInsertAfterPassages"></textarea>
-    </div>
   </div>  `
 })
 export class SummarySettingsComponent implements OnInit {
   @Input() config: SummarizerConfig;
 
   ngOnInit() {
-    if(!this.config.modelMaxTokens) {
+    if(!this.config.summarizationModel) {
       Object.assign(this.config, defaultSummarizerConfig);
     }
   }
--- projects/components/machine-learning/summary.component.ts
+++ projects/components/machine-learning/summary.component.ts
@@ -1,36 +1,41 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 import { JsonMethodPluginService, TopPassage } from "@sinequa/core/web-services";
 import { SearchService } from "@sinequa/components/search";
-import { BehaviorSubject, Observable, tap } from "rxjs";
+import { BehaviorSubject, map, Observable } from "rxjs";
 
 export interface SummarizerConfig {
   modelTemperature: number;
   modelTopP: number;
   modelMaxTokens: number;
 
-  removeOverlap: boolean;
-  removeDuplicates: boolean;
+  promptProtection: boolean;
+
+  summarizationModel: OpenAIModel;
   promptInsertBeforePassages: string;
-  promptInsertAfterPassages: string;
   extendBefore: number;
   extendAfter: number;
-  topPassages: number;
+  top: number;
 }
 
 export const defaultSummarizerConfig: SummarizerConfig = {
+  modelTemperature: 1.0,
+  modelTopP: 1.0,
+  modelMaxTokens: 800,
+  promptProtection: true,
+  summarizationModel: 'Davinci3',
+  promptInsertBeforePassages: `The below documents contains passages extracts returned by a search engine.
+  Your job is two perform 3 tasks:
+  1 - Try to answer the Query in one short sentence, you must add the reference to the document titles using markdown links. If you can't or don't have enough context or information from any documents to answer the query, just say so.
+  2 - Summarize all the documents at once in the context of the Query, using between 6 to 12 sentences. You should always generate a summary, even though there is no reference to the query and add the reference to the document titles using markdown links.
+  3 - After the summary, list all the documents in a list of bullet points titles using markdown links.
+  Markdown links must use the document title and document URL and follow this syntax: [Document Title](Document URL).
+  Query: {queryText}`,
   extendBefore: 0,
   extendAfter: 0,
-  removeOverlap: true,
-  removeDuplicates: true,
-  topPassages: 10,
-  modelTemperature: 0.7,
-  modelTopP: 1.0,
-  modelMaxTokens: 512,
-  promptInsertBeforePassages: "The below passages are extracts from documents returned by a search engine. Your job is to summarize these passages in order to provide an answer to the following query: {queryText}",
-  promptInsertAfterPassages: ""
+  top: 10,
 }
 
-export type OpenAIModel = "OpenAITextDavinci3" | "OpenAIgpt35Turbo";
+export type OpenAIModel = "Davinci3" | "GPT35Turbo";
 
 export type OpenAIModelMessage = {
   role: string;
@@ -105,7 +110,6 @@ export type OpenAIResponse = {
 export class SummaryComponent implements OnChanges {
   @Input() passages?: TopPassage[];
   @Input() config = defaultSummarizerConfig;
-  @Input() model: OpenAIModel = "OpenAIgpt35Turbo";
 
   @Output() data = new EventEmitter<OpenAIModelMessage>();
 
@@ -125,12 +129,14 @@ export class SummaryComponent implements OnChanges {
 
   ngOnChanges() {
     // Auto-migrate from older versions
-    if(!this.config.modelMaxTokens) {
+    if(!this.config.summarizationModel) {
       Object.assign(this.config, defaultSummarizerConfig);
     }
 
     if(this.passages) {
-      this.fetchData(this.passages);
+      this.loading = true;
+      this.messages$.next(undefined);
+      this.summarize(this.passages);
     }
     else {
       this.messages$.next(undefined);
@@ -140,58 +146,69 @@ export class SummaryComponent implements OnChanges {
 
   submitQuestion() {
     if(this.question.trim() && this.messages$.value) {
-      this.fetchAnswer(this.question.trim(), this.messages$.value);
+      this.loadingAnswer = true;
+      this.chat(this.messages$.value, this.question.trim());
     }
   }
 
   updateTokensPercentage() {
     if(this.tokens) {
       const questionTokens = Math.floor(this.question.trim().length / 3.5);
-      this.tokensAbsolute = (this.tokens.used || 0) + questionTokens;
+      this.tokensAbsolute = (this.tokens.used || 0) + questionTokens + this.config.modelMaxTokens;
       this.tokensPercentage = Math.min(100, 100 * this.tokensAbsolute / this.tokens.model);
     }
   }
 
-  private fetchData(passages: TopPassage[]): void {
-    this.loading = true;
-    this.messages$.next(undefined);
+  updateData(res: OpenAIResponse) {
+    this.messages$.next(res.messagesHistory);
+    this.data.emit(res.messagesHistory.at(-1));
+    this.loading = false;
+    this.loadingAnswer = false;
+    this.question = '';
+    this.tokens = res.tokens;
+    this.updateTokensPercentage();
+  }
+
+  public summarize(topPassages: TopPassage[]) {
     const queryText = this.searchService.query.text || '';
-    const data = {
-      ...this.config,
-      promptInsertBeforePassages: this.config.promptInsertBeforePassages.replace(/{queryText}/g, queryText),
-      promptInsertAfterPassages: this.config.promptInsertAfterPassages.replace(/{queryText}/g, queryText),
+    const model = {
+      model: this.config.summarizationModel,
+      temperature: this.config.modelTemperature,
+      generateTokens: this.config.modelMaxTokens,
+      topP: this.config.modelTopP
+    };
+    const passages = {
+      list: topPassages.map(p => ({docId: p.recordId, passageIndex: p.id, index: p.index})),
+      extendBefore: this.config.extendBefore,
+      extendAfter: this.config.extendAfter,
+      top: this.config.top
+    };
+    const data = { action: 'summarize', model, queryText, passages };
 
-      queryText,
-      passages: passages.map(p => ({docId: p.recordId, passageIndex: p.id, index: p.index}))
-    }
-    this.fetch(data).subscribe();
+    this.jsonMethodWebService.post('OpenAI', data)
+      .subscribe(res => this.updateData(res));
   }
 
-  private fetchAnswer(question: string, previousMessages: OpenAIModelMessage[]) {
-    this.loadingAnswer = true;
-    const data = {
-      ...this.config,
-      promptInsertBeforePassages: question,
-      promptInsertAfterPassages: '',
+  public chat(messages: OpenAIModelMessage[], question: string) {
+    const model = {
+      model: this.config.summarizationModel,
+      temperature: this.config.modelTemperature,
+      generateTokens: this.config.modelMaxTokens,
+      topP: this.config.modelTopP
+    };
+    const messagesHistory = [
+      ...messages,
+      {role: 'user', content: question, display: true}
+    ];
+    const data = { action: 'chat', model, promptProtection: this.config.promptProtection, messagesHistory };
 
-      previousMessages,
-      passages: [],
-      queryText: '...'
-    }
-    this.fetch(data).subscribe();
+    this.jsonMethodWebService.post('OpenAI', data)
+      .subscribe(res => this.updateData(res));
   }
 
-  private fetch(data: any): Observable<OpenAIResponse> {
-    return this.jsonMethodWebService.post(this.model, data).pipe(
-      tap((res: OpenAIResponse) => {
-        this.messages$.next(res.messagesHistory);
-        this.data.emit(res.messagesHistory.at(-1));
-        this.loading = false;
-        this.loadingAnswer = false;
-        this.question = '';
-        this.tokens = res.tokens;
-        this.updateTokensPercentage();
-      })
+  public countTokens(text: string, model: OpenAIModel): Observable<number> {
+    return this.jsonMethodWebService.post('OpenAI', {action: 'TokenCount', model, text}).pipe(
+      map(res => res.tokens)
     )
   }
 }
