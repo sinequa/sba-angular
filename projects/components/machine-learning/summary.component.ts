3af4ccdf9952627a7063c1fa86ef6a15f2698f76
Leibenguth <leibenguth@sinequa.com>
2023-03-14 17:11:07 +0100

    Added the capacity to display references below messages

--- projects/components/machine-learning/summary.component.ts
+++ projects/components/machine-learning/summary.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
-import { JsonMethodPluginService, TopPassage } from "@sinequa/core/web-services";
+import { JsonMethodPluginService, TopPassage, Record } from "@sinequa/core/web-services";
 import { SearchService } from "@sinequa/components/search";
 import { BehaviorSubject, map, Observable } from "rxjs";
+import { marked } from "marked";
 
 export interface SummarizerConfig {
   modelTemperature: number;
@@ -22,13 +23,12 @@ export const defaultSummarizerConfig: SummarizerConfig = {
   modelTopP: 1.0,
   modelMaxTokens: 800,
   promptProtection: true,
-  summarizationModel: 'Davinci3',
-  promptInsertBeforePassages: `The below documents contains passages extracts returned by a search engine.
-  Your job is two perform 3 tasks:
-  1 - Try to answer the Query in one short sentence, you must add the reference to the document titles using markdown links. If you can't or don't have enough context or information from any documents to answer the query, just say so.
-  2 - Summarize all the documents at once in the context of the Query, using between 6 to 12 sentences. You should always generate a summary, even though there is no reference to the query and add the reference to the document titles using markdown links.
-  3 - After the summary, list all the documents in a list of bullet points titles using markdown links.
-  Markdown links must use the document title and document URL and follow this syntax: [Document Title](Document URL).
+  summarizationModel: 'GPT35Turbo',
+  promptInsertBeforePassages: ` The below documents contains extracts returned by a search engine. Your job is two perform 2 tasks:
+  1 - Try to answer the Query in one short sentence. If you can't or don't have enough context or information from any documents to answer the query, just say so.
+  2 - Generate a single summary of all the documents in the context of the Query, using between 5 to 12 sentences.
+  Make sure you include the reference in the form [id].
+  Answer using using markdown syntax.
   Query: {queryText}`,
   extendBefore: 0,
   extendAfter: 0,
@@ -41,6 +41,8 @@ export type OpenAIModelMessage = {
   role: string;
   content: string;
   display: boolean;
+  $content: string;
+  $citations: OpenAIReference[];
 }
 
 export type OpenAIModelTokens = {
@@ -49,9 +51,17 @@ export type OpenAIModelTokens = {
   left: number;
 }
 
+export type OpenAIReference = {
+  refId: number;
+  recordId: string;
+  lEnginePassages: {passage_id: number; offset: number; length: number;}[];
+  $record: Record;
+}
+
 export type OpenAIResponse = {
   messagesHistory: OpenAIModelMessage[];
   tokens: OpenAIModelTokens;
+  scope?: OpenAIReference[];
 }
 
 @Component({
@@ -69,7 +79,17 @@ export type OpenAIResponse = {
           <i class="fas fa-2x fa-user-circle text-muted" *ngSwitchCase="'user'"></i>
           <i class="sq-chatgpt" [style.--sq-size.px]="28" *ngSwitchCase="'assistant'"></i>
         </span>
-        <div [innerHTML]="message.content | sqMarkdown"></div>
+        <div>
+          <div class="message-content" [innerHTML]="message.$content"></div>
+          <div *ngIf="message.$citations.length" class="citations small">
+            <b>Citations:</b>
+            <ul class="list-group">
+              <li *ngFor="let citation of message.$citations">
+                <span class="citation me-1">{{citation.refId}}</span> <a href="{{citation.$record.url1}}">{{citation.$record.title}}</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </li>
     </ng-container>
 
@@ -104,6 +124,16 @@ export type OpenAIResponse = {
     max-height: 100vh;
     font-size: 0.875rem;
   }
+  .citations .citation, .message-content ::ng-deep .citation{
+    font-size: 0.7em;
+    font-weight: bold;
+    position: relative;
+    bottom: 0.3em;
+    padding: 0 0.2em;
+    margin: 0 0.1em;
+    border-radius: 0.2em;
+    background-color: lightblue;
+  }
   `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -121,6 +151,8 @@ export class SummaryComponent implements OnChanges {
   tokensPercentage = 0;
   tokensAbsolute = 0;
   tokens?: OpenAIModelTokens;
+
+  scope?: OpenAIReference[];
 
   constructor(
     public jsonMethodWebService: JsonMethodPluginService,
@@ -160,6 +192,7 @@ export class SummaryComponent implements OnChanges {
   }
 
   updateData(res: OpenAIResponse) {
+    this.processMessages(res.messagesHistory, res.scope);
     this.messages$.next(res.messagesHistory);
     this.data.emit(res.messagesHistory.at(-1));
     this.loading = false;
@@ -167,6 +200,35 @@ export class SummaryComponent implements OnChanges {
     this.question = '';
     this.tokens = res.tokens;
     this.updateTokensPercentage();
+  }
+
+  processMessages(messages: OpenAIModelMessage[], scope?: OpenAIReference[]) {
+    if(scope) {
+      this.scope = scope;
+      this.scope.forEach(s => s.$record = this.searchService.results?.records.find(r => r.id === s.recordId)
+          || this.passages?.find(p => p.$record?.id === s.recordId)?.$record!)
+    }
+    for(let m of messages) {
+      m.$content = marked(m.content);
+      m.$citations = [];
+      if(m.role === 'assistant') {
+        const matches = m.$content.matchAll(/\[(\d)\]/g);
+        const citations = new Set<number>();
+        for(let match of matches) {
+          citations.add(+match[1]);
+        }
+        m.$citations = [...citations].sort((a,b)=> a-b)
+                                     .map(i => this.scope?.find(ref => ref.refId === i)!)
+                                     .filter(r => r);
+        m.$content = m.$content.replace(/\[(\d)\]/g, (str,ref) => {
+          const record = this.scope?.find(s => s.refId === +ref)?.$record;
+          if(record) {
+            return `<a class="citation" href="${record.url1}" title="${record.title}">${ref}</a>`;
+          }
+          return `<span class="citation">${ref}</a>`;
+        });
+      }
+    }
   }
 
   public summarize(topPassages: TopPassage[]) {
@@ -204,7 +266,7 @@ export class SummaryComponent implements OnChanges {
       topP: this.config.modelTopP
     };
     const messagesHistory = [
-      ...messages,
+      ...messages.map(m => ({role: m.role, content: m.content, display: m.display})), // Remove the volatile props ($content, $citations)
       {role: 'user', content: question, display: true}
     ];
     const data = {
