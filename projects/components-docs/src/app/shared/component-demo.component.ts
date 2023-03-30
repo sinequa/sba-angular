451a9010469f100665066680c7e106173ec73b1d
Leibenguth <leibenguth@sinequa.com>
2023-03-30 11:45:42 +0200

    Use sqFacetView for the doc-component-demo component

--- projects/components-docs/src/app/shared/component-demo.component.ts
+++ projects/components-docs/src/app/shared/component-demo.component.ts
@@ -1,44 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { Action } from '@sinequa/components/action';
 
 @Component({
   selector: 'doc-component-demo',
-  templateUrl: './component-demo.component.html',
-  styles: [`.nav-link {
-    cursor: pointer;
-  }
-
-  .nav-link:hover {
-    background-color: whitesmoke;
-  }
-
-  .nav-link.active {
-    font-weight: bold;
-    border-bottom: 3px solid var(--bs-nav-link-color);
-  }`]
+  templateUrl: './component-demo.component.html'
 })
 export class DocComponentDemoComponent {
 
   @Input() title: string;
   @Input() html: string;
   @Input() ts: string;
-
-  showCode = false;
-  codeView = 'html';
-
-  actions: Action[] = [
-    new Action({
-      icon: 'fas fa-code',
-      action: (action, event) => {
-        this.showCode = !this.showCode;
-        action.update();
-      },
-      updater: (action) => {
-        action.title = this.showCode ? 'Hide code' : 'View code';
-      }
-    })
-  ];
-
-  constructor() { }
 
 }
