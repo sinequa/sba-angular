fa07dbe0140859fc47f4a4629667bc207b119ac2
Leibenguth <leibenguth@sinequa.com>
2023-03-31 17:42:20 +0200

    Simplify result-duplicates component

--- projects/components/result/result-duplicates/result-duplicates.component.ts
+++ projects/components/result/result-duplicates/result-duplicates.component.ts
@@ -8,19 +8,12 @@ import { Record } from "@sinequa/core/web-services";
 })
 export class ResultDuplicatesComponent implements OnChanges {
   @Input() record: Record;
-  @Input() showList = false;
 
-  collapsed = true;
   exactDuplicates: number;
   nearDuplicates: number;
 
   ngOnChanges() {
-    this.collapsed = true; // Collapse on new record
     this.exactDuplicates = (this.record.groupcount || 1) - 1;
     this.nearDuplicates = this.record.$duplicateCount || 0;
-  }
-
-  toggle() {
-    this.collapsed = !this.collapsed;
   }
 }
