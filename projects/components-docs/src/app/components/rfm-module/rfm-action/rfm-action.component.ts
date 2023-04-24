2e647970582df87f48b8d404adabc24541fba6d3
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2023-04-24 09:46:23 +0200

    add mocks for rfm, selection, slide builder

--- projects/components-docs/src/app/components/rfm-module/rfm-action/rfm-action.component.ts
+++ projects/components-docs/src/app/components/rfm-module/rfm-action/rfm-action.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CCRFM, RFMType } from '@sinequa/components/rfm';
+import { AppService } from '@sinequa/core/app-utils';
+import { RFMActionDisplay } from '@sinequa/core/web-services';
 import { BaseComponent } from 'src/app/shared/base.component';
 
 @Component({
@@ -11,5 +14,25 @@ export class DocRfmActionComponent extends BaseComponent {
     [results]="results"
     [record]="record">
 </sq-rfm-action>`;
+
+config: CCRFM.Action = {
+  name: 'name',
+  enabled: true,
+  actionEnabled: true,
+  noMenu: false,
+  displayUnrated: true,
+  negAvailable: true
+};
+
+type: RFMType = 'click';
+
+get rfm(): RFMActionDisplay | undefined {
+  const rfm = this.globalService.record?.rfm;
+  return rfm ? rfm[this.type] : undefined;
+}
+
+  constructor(public appService: AppService) {
+    super();
+  }
 
 }
