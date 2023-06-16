bba6e942f57d147aba839d69e0e4a2e3f4206797
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2023-06-16 11:18:34 +0200

    create README for cypress + few optimizations

--- projects/components-docs/src/environments/environment.ts
+++ projects/components-docs/src/environments/environment.ts
@@ -1,7 +1,11 @@
+import { CREDENTIALS } from '@sinequa/core/login';
+
 export const environment = {
   url: "https://localhost:4200",
   autoSAMLProvider: "identity-dev",
   production: false,
   mock: false,
-  providers: []
+  providers: [
+    { provide: CREDENTIALS, useValue: { userName: '', password: '' } }
+  ]
 };
