2061324553605952b8b6fcbdab319eb05209ac5f
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2023-06-22 14:28:25 +0200

    refactor icons mappings to be handled in ts

--- projects/components/result/result-icon/result-icon.ts
+++ projects/components/result/result-icon/result-icon.ts
@@ -1,23 +1,57 @@
-import {Component, Input} from "@angular/core";
-import {Record} from "@sinequa/core/web-services";
-
-/**
- * This component requires a global CSS file to map file extensions (doc, ppt, xls, etc.)
- * to Font Awesome icons via a class `sq-icon-file-{{fileext}}`
- */
+import { Component, Input, OnChanges } from "@angular/core";
+import { Record } from "@sinequa/core/web-services";
+import { defaultFormatIcons } from "./icons";
 
 @Component({
     selector: "sq-result-icon",
     templateUrl: "./result-icon.html"
 })
-export class ResultIcon {
+export class ResultIcon implements OnChanges {
     /**
      * Record which file extension is displayed as an icon
      */
     @Input() record: Record;
 
     /**
+     * Any forced icon to fetch from the mappings list
+     */
+    @Input() icon?: string;
+
+    /**
+     * Any icons overriding elements
+     * This will still use the default icons but will patch it with the provided data
+     */
+    @Input() set formatIcons(icons) {
+        this._formatIcons = { ...defaultFormatIcons, ...icons }
+    }
+
+    get formatIcons() {
+        return this._formatIcons;
+    }
+
+    /**
      * Integer controlling the icon's size
      */
-    @Input() size: number = 1; 
+    @Input() size: number = 1;
+
+    /**
+     * Whether the icon should be colorized
+     */
+    @Input() colorize: boolean = true;
+
+    private _formatIcons = defaultFormatIcons;
+
+    iconClass: string | undefined;
+    color: string | undefined;
+
+    ngOnChanges(): void {
+        this.iconClass = undefined;
+        this.color = undefined;
+
+        if (this.record || this.icon) {
+            const mapping = this.formatIcons[this.icon || this.record.fileext] || this.formatIcons.file;
+            this.iconClass = `${mapping.style} fa-${mapping.icon}`;
+            this.color = this.colorize ? mapping.color : undefined;
+        }
+    }
 }
