5b33d9047e54ac933d555ea9fc7a1712334e0616
Leibenguth <leibenguth@sinequa.com>
2023-07-19 20:39:00 +0200

    Implement the placeholder in new chat message component

--- projects/components/machine-learning/chat/chat-message.component.ts
+++ projects/components/machine-learning/chat/chat-message.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from "@angular/core";
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ChangeDetectorRef } from "@angular/core";
 import { Record } from "@sinequa/core/web-services";
 import { SearchService } from "@sinequa/components/search";
 import { ChatAttachment, ChatMessage } from "./types";
 
-import { unified, Plugin, Processor } from "unified";
+import { unified, Processor } from "unified";
 import remarkParse from "remark-parse";
-import { visit, CONTINUE } from "unist-util-visit";
+import { visit, CONTINUE, EXIT } from "unist-util-visit";
 import { Text, Parent, Content } from "mdast";
 import { Node } from "unist";
 
@@ -19,6 +19,7 @@ export class ChatMessageComponent implements OnChanges {
   @Input() message: ChatMessage;
   @Input() conversation: ChatMessage[];
   @Input() assistantIcon: string;
+  @Input() streaming: boolean;
   @Output() referenceClicked = new EventEmitter<Record>();
 
   processor: Processor;
@@ -27,7 +28,8 @@ export class ChatMessageComponent implements OnChanges {
   referenceMap = new Map<number, ChatAttachment>();
 
   constructor(
-    public searchService: SearchService
+    public searchService: SearchService,
+    public cdr: ChangeDetectorRef
   ) {}
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -39,9 +41,14 @@ export class ChatMessageComponent implements OnChanges {
           this.referenceMap.set(m.$refId!, m.$attachment);
         }
       }
+
       this.processor = unified()
         .use(remarkParse)
-        .use(this.referencePlugin);
+        .use(() => this.referencePlugin);
+
+      if(this.streaming) {
+        this.processor = this.processor.use(() => this.placeholderPlugin);
+      }
     }
   }
 
@@ -57,63 +64,71 @@ export class ChatMessageComponent implements OnChanges {
    * This Unified plugin looks a text nodes and replaces any reference in the
    * form [1], [2,3,4], [3-5], etc. with custom nodes of type "chat-reference".
    */
-  referencePlugin: Plugin = () => {
+  referencePlugin = (tree: Node) => {
 
-    return (tree: Node) => {
+    const references = new Set<number>();
 
-      const references = new Set<number>();
+    // Visit all text nodes
+    visit(tree, "text", (node: Text, index: number, parent: Parent) => {
+      const text = node.value;
 
-      // Visit all text nodes
-      visit(tree, "text", (node: Text, index: number, parent: Parent) => {
-        const text = node.value;
+      const matches = this.getReferenceMatches(text);
 
-        const matches = this.getReferenceMatches(text);
-
-        // Quit if no references were found
-        if(matches.length === 0) {
-          return CONTINUE;
-        }
-
-        const nodes: (Content & {end: number})[] = [];
-
-        for(let match of matches) {
-          const refId = +match[1];
-          // We find a valid reference in the text
-          if(!isNaN(refId)) {
-            references.add(refId); // Add it to the set of used references
-
-            // If needed, insert a text node before the reference
-            const current = nodes.at(-1) ?? {end: 0};
-            if (match.index! > current.end) {
-              nodes.push({ type: "text", value: text.substring(current.end, match.index!), end: match.index! });
-            }
-
-            // Add a custom reference node
-            nodes.push({ type: "chat-reference", refId, end: match.index! + match[0].length } as any);
-          }
-        }
-
-        // Quit if no references were found
-        if(nodes.length === 0) {
-          return CONTINUE;
-        }
-
-        if(nodes.at(-1)!.end < text.length) {
-          nodes.push({ type: "text", value: text.substring(nodes.at(-1)!.end, text.length), end: text.length });
-        }
-
-        // Delete the current text node from the parent and replace it with the new nodes
-        parent.children.splice(index, 1, ...nodes);
-
-        return index + nodes.length; // Visit the next node after the inserted ones
-      });
-
-      if(references.size > 0) {
-        this.references = Array.from(references.values()).sort((a,b) => a-b);
+      // Quit if no references were found
+      if(matches.length === 0) {
+        return CONTINUE;
       }
 
-      return tree;
+      const nodes: (Content & {end: number})[] = [];
+
+      for(let match of matches) {
+        const refId = +match[1];
+        // We find a valid reference in the text
+        if(!isNaN(refId)) {
+          references.add(refId); // Add it to the set of used references
+
+          // If needed, insert a text node before the reference
+          const current = nodes.at(-1) ?? {end: 0};
+          if (match.index! > current.end) {
+            nodes.push({ type: "text", value: text.substring(current.end, match.index!), end: match.index! });
+          }
+
+          // Add a custom reference node
+          nodes.push({ type: "chat-reference", refId, end: match.index! + match[0].length } as any);
+        }
+      }
+
+      // Quit if no references were found
+      if(nodes.length === 0) {
+        return CONTINUE;
+      }
+
+      if(nodes.at(-1)!.end < text.length) {
+        nodes.push({ type: "text", value: text.substring(nodes.at(-1)!.end, text.length), end: text.length });
+      }
+
+      // Delete the current text node from the parent and replace it with the new nodes
+      parent.children.splice(index, 1, ...nodes);
+
+      return index + nodes.length; // Visit the next node after the inserted ones
+    });
+
+    if(references.size > 0) {
+      this.references = Array.from(references.values()).sort((a,b) => a-b);
+      this.cdr.detectChanges();
     }
+
+    return tree;
+  }
+
+  placeholderPlugin = (tree: Node) => {
+
+    visit(tree, "text", (node: Text, index: number, parent: Parent) => {
+      parent.children.push({type: "streaming-placeholder"} as any);
+      return EXIT;
+    }, true);
+
+    return tree;
   }
 
   /**
