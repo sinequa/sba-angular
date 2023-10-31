60d59b88537b0e8df30c18c7bfbd20817116037e
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2023-10-31 14:52:05 +0100

    Add new search form toggle for filters persistance

--- projects/components/search-form/messages/en.ts
+++ projects/components/search-form/messages/en.ts
@@ -5,6 +5,8 @@ export default {
     "clearAll": "Clear Search",
     "clear": "Clear",
     "voiceRecognition": "Vocal search",
+    "persistedFiltersOn": "Filters kept upon new seach",
+    "persistedFiltersOff": "Filters removed upon new seach",
     "listening": "Listening in progress...",
     "filterCount": "{value} {value, plural, one {active filter} other {active filters}} (click to clear)",
     "apply": "Apply",
