d824c4509e1e48b3680e22c37ce3ab4a7ff3219d
Tome <olivier.tome@sinequa.com>
2024-10-08 12:58:44 +0200

    🐛 ES-24206 (search input) fielded search with regex pattern doesn't work (#1208) #done
    #done
    ## Search Service
    - refactor `loadmore()`
    
    ## Search Form component
    ### Code
    - changed template's model from to reactive form's model
    - added `onInputChange()` function to parse and check the validity of the input. If the expression is wrong, the search's input will be surrounded in red, otherwise it's surrounded in blue.
    
    ### Template
    - removed `[(ngModel)]` because form is now managed by code
    - changed DOM structure with `<form/>` and `<fieldset/>` blocks
    -  moved `disabled` attribute to the parent `fieldset` block. This block disable all children elements automatically
    - added (ngModelChange)` callback to parse the input to check it's validity
    
    ### CSS
    - added css rule when form's controls are disabled
    - added css rule when search input is invalid

--- projects/components/search/search.service.ts
+++ projects/components/search/search.service.ts
@@ -806,7 +806,7 @@ export class SearchService<T extends Results = Results> implements OnDestroy {
                 this.getResults(this.query, auditEvents)
                     .subscribe(results => {
                         if(this.results && results) {
-                            this.results.records = [...this.results?.records || [], ...results.records] || [];
+                            this.results.records = [...(this.results?.records ?? []), ...results.records];
                             this._resultsStream.next(this.results);
                         }
                         this.fetchingLoadMore = false;
