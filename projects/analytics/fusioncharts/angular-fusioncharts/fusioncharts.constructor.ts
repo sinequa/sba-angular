570dec07ac7e7c67571a3940c12d05410b5beb8e
TOME Olivier <olivier.tome@sinequa.com>
2025-04-18 09:16:05 +0200

    ✨ ES-26531 #done feat: license fetching

--- projects/analytics/fusioncharts/angular-fusioncharts/fusioncharts.constructor.ts
+++ projects/analytics/fusioncharts/angular-fusioncharts/fusioncharts.constructor.ts
@@ -2,6 +2,23 @@
 export function FusionChartsConstructor(fusionchartsService: any, chartConfig: Object) {
     const FusionCharts = fusionchartsService.getFusionChartsStatic();
 
+
+    getLicense().then((license: any) => {
+        FusionCharts['options'].license(license);
+    }
+    ).catch((error: any) => {
+        console.error("Error fetching license:", error);
+        // Handle the error as needed
+    });
+
     return new FusionCharts(chartConfig);
+}
+
+async function getLicense() {
+    const response = await fetch("r/fusioncharts.json");
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
 }
 
